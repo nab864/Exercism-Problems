9defa2953e835cbe0e158000320291f4bdfd0aa3
Kai <kaifuller1995@gmail.com>
2023-05-04 10:50:44 -0600

    added requirements.txt

--- Problems/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-website_url = requests.get('https://en.wikipedia.org/wiki/Demographics_of_Italy').text
-
-from bs4 import BeautifulSoup
-soup = BeautifulSoup(website_url,'lxml')
-#print(soup.prettify())
-
-My_table = soup.find_all('table', attrs={"class": "wikitable sortable"})
